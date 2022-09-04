f3eb467f89a5bdfe6a9f58e771ad72ac2fded2e4
Ana Cristina de Magalhães Araújo (Kika) <105136454+kikamagalhaes@users.noreply.github.com>
2022-09-04 19:21:27 -0300

    commitando

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class DIO {
+        
+static void Main(string[] args){
+
+        int numero = int.Parse(Console.ReadLine());
+        int produto = 1, soma = 0;
+        while (numero > 0){
+             int digito = numero % 10;  //para encontrar o último dígito
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+             produto = produto * digito;
+             soma = soma + digito;
+             numero = numero /10    ;
+           }
+
+           Console.WriteLine(produto-soma);
+        }
+    }
